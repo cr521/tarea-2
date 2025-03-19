90ac2f6fb37f96d8c6187e81bf59ef1933cb0d5f
cr521 <cristhiancumayen@gmail.com>
2025-03-18 23:09:07 -0600

    Add files via upload

---
+++ numero positivo mayor0.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+main (){
+	//numero positivo>0 numero negativo<0
+	int num=0;
+	cout<<"ingrese el numero:";
+	cin>>num;
+	if(num>0){
+		//entra aca cuando la condicion sea verdadera
+		cout<<"Positivo:"<<endl;	
+	}else{
+	if(num==0){
+	cout<<"Neutro:"<<endl;	
+	
+	}else{
+	cout<<"Negativo:"<<endl;
+    }
+	
+}
+	system("pause");
+}
